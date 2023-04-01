e7e5ad22f9f99b597957482f9d3c67c07168c5a4
sylar-codex <vicaremy@gmail.com>
2023-04-01 16:17:54 +0100

    removed allowed host

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOST = ['https://jum-jacket-store-backend-production.up.railway.app']
+ALLOWED_HOST = []
 
 
 # Application definition
