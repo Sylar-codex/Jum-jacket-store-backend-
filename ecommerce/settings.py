e16dd46ec3514c993ab642b14ac4021dc67080a4
sylar-codex <vicaremy@gmail.com>
2023-04-22 13:15:57 +0100

    cors

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -143,10 +143,10 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-# CORS_ALLOWED_ORIGINS = [
-#      'http://localhost:3000',
-#      "https://glistening-tartufo-85e110.netlify.app/"
-# ]
+CORS_ALLOWED_ORIGINS = [
+     'http://localhost:3000',
+     "https://glistening-tartufo-85e110.netlify.app"
+]
 
 CORS_ALLOW_ALL_ORIGINS: True
 CORS_ALLOW_CREDENTIALS = True
