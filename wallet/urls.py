8499bca127a7c1706b8c5f7361baa28105c3ca42
sylar-codex <vicaremy@gmail.com>
2024-01-09 20:15:40 +0100

    paystack webhook

--- wallet/urls.py
+++ wallet/urls.py
@@ -1,10 +1,10 @@
 from django.urls import include,path
 
-from .api import DepositFundsAPI, WalletInfoAPI,VerifyDepositAPI
+from .api import DepositFundsAPI, WalletInfoAPI,VerifyDepositAPI,PaystackWebhookView
 
 urlpatterns = [
     path('api/wallet_info', WalletInfoAPI.as_view()),
     path('api/deposit', DepositFundsAPI.as_view()),
     path('api/deposit/verify/<str:reference>/',VerifyDepositAPI.as_view()),
-    path('api/paystack_webhook/',VerifyDepositAPI.as_view()),
+    path('api/paystack_webhook/',PaystackWebhookView.as_view()),
 ]
